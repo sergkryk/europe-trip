08408481e852452f38623f6778685d21a6f11df3
Serge Krykov <sergkryk@Serges-iMac.local>
2020-03-21 15:17:09 +0300

    добавляет переключение табов

--- build/js/script.js
+++ build/js/script.js
@@ -1,15 +1,42 @@
 'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// var pageHeader = document.querySelector('.page-header');
+// var headerToggle = document.querySelector('.page-header__toggle');
 
-pageHeader.classList.remove('page-header--nojs');
+// pageHeader.classList.remove('page-header--nojs');
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+// headerToggle.addEventListener('click', function () {
+//   if (pageHeader.classList.contains('page-header--closed')) {
+//     pageHeader.classList.remove('page-header--closed');
+//     pageHeader.classList.add('page-header--opened');
+//   } else {
+//     pageHeader.classList.add('page-header--closed');
+//     pageHeader.classList.remove('page-header--opened');
+//   }
+// });
+
+var tabs = document.querySelectorAll('.country__tab');
+var sections = document.querySelectorAll('.country__details');
+
+tabs.forEach(function (tab) {
+  tab.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    removeActiveTab();
+    addActiveTab(tab);
+  });
 });
+
+var removeActiveTab = function () {
+  tabs.forEach(function (tab) {
+    tab.classList.remove('country__tab--active');
+  });
+  sections.forEach(function (section) {
+    section.classList.remove('country__details--active');
+  });
+};
+
+var addActiveTab = function (tab) {
+  tab.classList.add('country__tab--active');
+  var href = tab.getAttribute('href');
+  var matchingSection = document.querySelector(href);
+  matchingSection.classList.add('country__details--active');
+};
